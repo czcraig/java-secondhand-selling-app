fdc0de9704615c8595f31343ea7a34c564b2bb99
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-19 17:01:21 +0100

    get routes for ads index and show

--- src/main/java/controllers/AdvertsController.java
+++ src/main/java/controllers/AdvertsController.java
@@ -1,4 +1,48 @@
 package controllers;
 
+import db.DBHelper;
+import models.Advert;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+
 public class AdvertsController {
+
+    public AdvertsController() {
+        this.setupEndPoints();
+    }
+
+    public void setupEndPoints() {
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+
+        get("/adverts", (request, response) -> {
+            Map<String, Object> model = new HashMap();
+            model.put("template", "templates/adverts/index.vtl");
+
+
+            List<Advert> adverts = DBHelper.getAll(Advert.class);
+            model.put("adverts", adverts);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
+
+
+        get("/adverts/:id", (request, response) -> {
+
+            Map<String, Object> model = new HashMap<>();
+
+            model.put("template", "templates/adverts/show.vtl");
+            int advertId = Integer.parseInt(request.params(":id"));
+            Advert advert = DBHelper.find(Advert.class, advertId);
+            model.put("advert", advert);
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, velocityTemplateEngine);
+    }
+
 }
