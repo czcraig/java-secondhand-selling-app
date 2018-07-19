150b79c4cb4651dc02a627bcfff9272e4fe88390
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-19 14:51:09 +0100

    saved data in db

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -11,11 +11,15 @@ public class Seeds {
         DBHelper.deleteAll(User.class);
 
         User user1 = new User("Bill", "bill@hotmail.com");
+        DBHelper.save(user1);
         User user2 = new User("Jess", "jess@gmail.com");
+        DBHelper.save(user2);
 
         Advert advert1 = new Advert("Bike for sale", "Hardly used", CategoryType.BICYLE, 35.00, "Edinburgh", user1, "public/images/bike.jpg", "For Sale");
+        DBHelper.save(advert1);
 
         Advert advert2 = new Advert("Guitar for Sale", "Hardly used", CategoryType.INSTRUMENT, 85.00, "Edinburgh", user1, "public/images/guitar.jpg", "For Sale");
+        DBHelper.save(advert2);
 
 
 
