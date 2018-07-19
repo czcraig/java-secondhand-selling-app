385f973e932acb78cf8832ef4c5880710570386e
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-19 13:51:09 +0100

    advert class set up

--- src/main/java/models/Advert.java
+++ src/main/java/models/Advert.java
@@ -1,7 +1,9 @@
 package models;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
-
+@Entity
+@Table(name="adverts")
 public class Advert {
 
 private int id;
@@ -16,6 +18,8 @@ private String image_url;
 private String ad_status;
 
 
+    public Advert() {
+    }
 
     public Advert(String title, String description, CategoryType category, double price, Timestamp date, String location, User seller, String image_url, String ad_status) {
         this.title = title;
@@ -26,6 +30,99 @@ private String ad_status;
         this.location = location;
         this.seller = seller;
         this.image_url = image_url;
+        this.ad_status = ad_status;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name= "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "category")
+    public CategoryType getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryType category) {
+        this.category = category;
+    }
+
+    @Column(name = "price")
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Column(name = "date")
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    @Column(name = "location")
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    @Column(name = "image_url")
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    @Column(name = "ad_status")
+    public String getAd_status() {
+        return ad_status;
+    }
+
+    public void setAd_status(String ad_status) {
         this.ad_status = ad_status;
     }
 }
