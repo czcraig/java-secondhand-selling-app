063307837a69d394b61818951f8799ac2a4c0a1d
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-20 15:36:03 +0100

    buy and sell homepage views

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -3,9 +3,11 @@ package controllers;
 import db.DBHelper;
 import db.Seeds;
 import models.Advert;
+import models.CategoryType;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +29,21 @@ public class Controller {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap();
             model.put("template", "templates/home.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/buy", (request, response) -> {
+            Map<String, Object> model = new HashMap();
+            List<CategoryType> categories = Arrays.asList(CategoryType.values());
+            model.put("categories", categories);
+            model.put("template", "templates/buy.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+
+        get("/sell", (request, response) -> {
+            Map<String, Object> model = new HashMap();
+            model.put("template", "templates/sell.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
